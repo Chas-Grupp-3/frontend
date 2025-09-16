9c99be29b3e7bf9eef9fd30178706a6e36bba7c0
Wicked_Colours <sandra.w.l@gmail.com>
2025-09-16 15:43:04 +0200

    start of a button?

--- packages/UI/src/Button.tsx
+++ packages/UI/src/Button.tsx
@@ -1,11 +1,53 @@
+import styled from "styled-components";
+import { colors, radius } from "./styles";
+import Text from "./font";
+
 interface ButtonProps {
   label: string;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
+  buttonStyle: buttonStyle;
 }
+type buttonStyle = "primary" | "secondary" | "disabled" | "destructive";
+interface styledButtonProps {
+  $buttonStyle: buttonStyle;
+}
+const StyledButton = styled.button<styledButtonProps>`
+  border-radius: ${radius.button};
+  padding: 8px 16px;
+  border: 2px solid ${colors.primary};
+  ${({ $buttonStyle }) =>
+    $buttonStyle === "primary"
+      ? `
+      background-color: ${colors.primary};
+      &:hover{
+      background-color: ${colors.buttonHover};
+      }
+    `
+      : `
+    background-color: ${colors.background};
+    `}
+`;
 
-export const Button = ({ label, onClick, type = "button" }: ButtonProps) => (
-  <button type={type} onClick={onClick}>
-    {label}
-  </button>
-);
+export const Button = ({
+  label,
+  onClick,
+  type = "button",
+  buttonStyle = "primary",
+}: ButtonProps) => {
+  const textColor: keyof typeof colors =
+    buttonStyle === "primary"
+      ? colors.accent
+      : buttonStyle === "secondary"
+        ? colors.primary
+        : buttonStyle === "disabled"
+          ? colors.greyText
+          : colors.pause;
+  return (
+    <StyledButton type={type} onClick={onClick} $buttonStyle={buttonStyle}>
+      <Text size="Button" color={textColor}>
+        {label}
+      </Text>
+    </StyledButton>
+  );
+};
