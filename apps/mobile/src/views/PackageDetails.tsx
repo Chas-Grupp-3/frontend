6263f84551ab609eca1ea0b6feda8af0fb27d05d
Alex Raunert <alexander.raunert@gmail.com>
2025-10-01 14:39:33 +0200

    added views

---
+++ apps/mobile/src/views/PackageDetails.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@chas/ui";
+const PackageDetails = () => {
+  return (
+    <div>
+      <Text size="H1">Package Details</Text>
+    </div>
+  );
+};
+export default PackageDetails;
