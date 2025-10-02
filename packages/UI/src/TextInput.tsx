2343c0f53df55aa04228e2f4bc7ce240fb1f6c2a
aiwa-L <a.i.w.a@live.se>
2025-10-02 09:47:22 +0200

    Update packages/UI/src/TextInput.tsx
    Co-authored-by: Alex Raunert <alexander.raunert@gmail.com>

--- packages/UI/src/TextInput.tsx
+++ packages/UI/src/TextInput.tsx
@@ -1,4 +1,5 @@
-import React, { InputHTMLAttributes, forwardRef } from "react";
+import { forwardRef } from "react";
+import type { InputHTMLAttributes } from "react";
 import styled, { css } from "styled-components";
 import { colors, radius, textWeb } from "./styles";
 
