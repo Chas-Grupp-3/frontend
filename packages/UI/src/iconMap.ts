5da1300719016103e673b43a82d256e6359f51cd
Wicked_Colours <sandra.w.l@gmail.com>
2025-10-14 21:47:30 +0200

    bigTemp icon

--- packages/UI/src/iconMap.ts
+++ packages/UI/src/iconMap.ts
@@ -2,11 +2,13 @@ import packageIcon from "../assets/icons/package.svg";
 import smallTemp from "../assets/icons/smallTemp.svg";
 import qrScan from "../assets/icons/qrScan.svg";
 import hamburger from "../assets/icons/hamburger.svg";
+import bigTemp from "../assets/icons/bigTemp.svg";
 
 export const iconMap = {
   package: packageIcon,
   smallTemp: smallTemp,
   qrScan: qrScan,
+  bigTemp: bigTemp,
   hamburger: hamburger,
 } as const;
 
