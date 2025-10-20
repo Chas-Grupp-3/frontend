6bed7bcff0c64ee51980bbbb2286675cbada3ca3
Alex Raunert <alexander.raunert@gmail.com>
2025-10-20 14:56:47 +0200

    login cleanup, use logo compoent and login form component

--- apps/mobile/src/views/Login.tsx
+++ apps/mobile/src/views/Login.tsx
@@ -1,74 +1,41 @@
-import { Text, TextInput, Button, colors, Icon } from "@chas/ui";
+import { Text, colors, Icon } from "@chas/ui";
 import styled from "styled-components";
+import LoginForm from "../components/LoginForm";
+import Logo from "../components/Logo";
 
 const Login = () => {
   return (
     <Container className="page">
-      <Card>
-        <Logo>
-          <Icon name="smallTempHot" size={64} alt="ThermoTrack Logo" />
-          <LogoText>
-            <Text variant="h2">ThermoTrack</Text>
-            <Text variant="body-sm">Climate-Controlled Transport</Text>
-          </LogoText>
-        </Logo>
-        <Title>
-          <Text variant="h1">Log in</Text>
-        </Title>
+      <Logo />
+      <StyledForm>
+        <Text variant="h2">Log in</Text>
         <Divider />
-        <Form>
-          <TextInput label="E-mail" />
-          <TextInput label="Password" type="password" />
-          <Text variant="body-smBold">Forgot password?</Text>
-          <ButtonContainer>
-            <Button buttonVariant="primary">Log in</Button>
-          </ButtonContainer>
-        </Form>
-        <CreateUser>
-          <Text variant="body-sm">New user?</Text>
-          <Text variant="body-smBold">Create account</Text>
-        </CreateUser>
-        <Footer>
-          <Icon name="truckPin" alt="TruckPin Logo" size={300} />
-        </Footer>
-      </Card>
+        <LoginForm />
+      </StyledForm>
+      {/* <CreateUser>
+        <Text variant="body-sm">New user?</Text>
+        <Text variant="body-smBold">Create account</Text>
+      </CreateUser> */}
+      <Footer>
+        <Icon name="truckPin" alt="TruckPin Logo" size={300} />
+      </Footer>
     </Container>
   );
 };
 export default Login;
 
 const Container = styled.div`
-  display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+  padding: 2rem;
+  background-color: ${colors.background};
 `;
 
-const Card = styled.div``;
-
-const Logo = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  margin-bottom: 1rem;
-`;
-
-const LogoText = styled.div`
+const StyledForm = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  text-align: left;
-  align-items: flex-start;
-  color: ${colors.primary};
-`;
-
-const Title = styled.h1`
-  display: flex;
-  justify-content: center;
+  width: 100%;
   text-align: center;
-  font-size: 2rem;
-  margin-bottom: 1rem;
 `;
 
 const Divider = styled.div`
@@ -78,31 +45,16 @@ const Divider = styled.div`
   margin: 1rem 0;
 `;
 
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-  width: 100%;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-`;
-
-const CreateUser = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  margin-top: 1rem;
-  flex-direction: row;
-`;
+// const CreateUser = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 1rem;
+//   margin-top: 1rem;
+//   flex-direction: row;
+// `;
 
 const Footer = styled.footer`
-  display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 3rem;
