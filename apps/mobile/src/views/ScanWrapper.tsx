822882e14b632aad7659b33c42782448483304fe
Alex Raunert <alexander.raunert@gmail.com>
2025-10-30 10:30:11 +0100

    optinally add id to scan

--- apps/mobile/src/views/ScanWrapper.tsx
+++ apps/mobile/src/views/ScanWrapper.tsx
@@ -2,13 +2,15 @@ import { useParams } from "react-router";
 import Scan from "../components/Scan";
 
 const ScanWrapper = () => {
-  const { mode } = useParams<{ mode: "search" | "add" | "deliver" }>();
-
+  const { mode, id } = useParams<{
+    mode: "add" | "deliver" | "search";
+    id?: string;
+  }>();
   if (!mode || !["search", "add", "deliver"].includes(mode)) {
     return <div>Invalid scan mode</div>;
   }
 
-  return <Scan mode={mode} />;
+  return <Scan mode={mode} packageId={id} />;
 };
 
 export default ScanWrapper;
