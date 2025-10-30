9ce9c13a91038bd4fb5f15720db1ce434a8327aa
Wicked_Colours <sandra.w.l@gmail.com>
2025-10-30 20:26:01 +0100

    alt and aria

--- apps/mobile/src/views/driver/DriverProfile.tsx
+++ apps/mobile/src/views/driver/DriverProfile.tsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
 import ProfileInfo from "../../components/Profile/ProfileInfo";
 import ProfileHeader from "../../components/Profile/ProfileHeader";
 
 const Profile = () => {
   return (
-    <div className="page">
+    <div
+      className="page"
+      role="main"
+      aria-labelledby="page-title"
+      aria-describedby="page-description"
+    >
+      <ScreenReaderOnly id="page-title">Driver Profile</ScreenReaderOnly>
+
+      <ScreenReaderOnly id="page-description">
+        Show and handle your profile as a driver. Here you can see your
+        information and log out.
+      </ScreenReaderOnly>
+
       <ProfileHeader />
       <ProfileInfo />
     </div>
@@ -11,3 +24,15 @@ const Profile = () => {
 };
 
 export default Profile;
+
+const ScreenReaderOnly = styled.div`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+`;
--- apps/mobile/src/views/user/UserProfile.tsx
+++ apps/mobile/src/views/user/UserProfile.tsx
@@ -1,9 +1,24 @@
+import styled from "styled-components";
 import ProfileHeader from "../../components/Profile/ProfileHeader";
 import ProfileInfo from "../../components/Profile/ProfileInfo";
 
 const Profile = () => {
   return (
-    <div className="page">
+    <div
+      className="page"
+      role="main"
+      aria-labelledby="page-title"
+      aria-describedby="page-description"
+    >
+      <ScreenReaderOnly as="h1" id="page-title">
+        Customer Profile
+      </ScreenReaderOnly>
+
+      <ScreenReaderOnly id="page-description">
+        Show and handle your profile as a customer. Here you can see your
+        information and log out.
+      </ScreenReaderOnly>
+
       <ProfileHeader />
       <ProfileInfo />
     </div>
@@ -11,3 +26,15 @@ const Profile = () => {
 };
 
 export default Profile;
+
+const ScreenReaderOnly = styled.div`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+`;
