77eda4fc79d40ecc8148bc2114a284d7db4d5342
Wicked_Colours <sandra.w.l@gmail.com>
2025-10-31 10:33:03 +0100

    Deleted SR

--- apps/mobile/src/views/driver/DriverProfile.tsx
+++ apps/mobile/src/views/driver/DriverProfile.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import ProfileInfo from "../../components/Profile/ProfileInfo";
 import ProfileHeader from "../../components/Profile/ProfileHeader";
 
@@ -10,13 +9,6 @@ const Profile = () => {
       aria-labelledby="page-title"
       aria-describedby="page-description"
     >
-      <ScreenReaderOnly id="page-title">Driver Profile</ScreenReaderOnly>
-
-      <ScreenReaderOnly id="page-description">
-        Show and handle your profile as a driver. Here you can see your
-        information and log out.
-      </ScreenReaderOnly>
-
       <ProfileHeader />
       <ProfileInfo />
     </div>
@@ -24,15 +16,3 @@ const Profile = () => {
 };
 
 export default Profile;
-
-const ScreenReaderOnly = styled.div`
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  white-space: nowrap;
-  border: 0;
-`;
