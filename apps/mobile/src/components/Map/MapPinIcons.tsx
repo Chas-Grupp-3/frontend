7cb88547f1264647239bd51d9da2c78f056c59d0
Wicked_Colours <sandra.w.l@gmail.com>
2025-11-03 22:00:05 +0100

    WCAG MapPinIcons.tsx

--- apps/mobile/src/components/Map/MapPinIcons.tsx
+++ apps/mobile/src/components/Map/MapPinIcons.tsx
@@ -3,13 +3,28 @@ import ReactDOMServer from "react-dom/server";
 import { Icon } from "@chas/ui";
 
 const greenIconHtml = ReactDOMServer.renderToStaticMarkup(
-  <Icon name="greenPin" size="md" />
+  <Icon
+    name="greenPin"
+    size="md"
+    aria-label="Green location pin"
+    alt="Green map pin icon"
+  />
 );
 const redIconHtml = ReactDOMServer.renderToStaticMarkup(
-  <Icon name="redPin" size="md" />
+  <Icon
+    name="redPin"
+    size="md"
+    aria-label="Red location pin"
+    alt="Red map pin icon"
+  />
 );
 const yellowIconHtml = ReactDOMServer.renderToStaticMarkup(
-  <Icon name="yellowPin" size="md" />
+  <Icon
+    name="yellowPin"
+    size="md"
+    aria-label="Yellow location pin"
+    alt="Yellow map pin icon"
+  />
 );
 
 export const greenPinIcon = L.divIcon({
